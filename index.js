77b32babc013b4fe4e56cd7756e2891f602b191f
kay.bestmann <kay.bestmann@accenture.com>
2023-02-16 09:50:07 +0100

    cleanup stuff

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const uuid = require('uuid');
+
 const connection = mongoose.createConnection(
   "mongodb://localhost:27017/mycollection"
 );
@@ -9,11 +11,14 @@ const connection = mongoose.createConnection(
       name: {type: String ,required: true},
       photos: [String],
       status: {type: String},
-    })
+    }
+     , {optimisticConcurrency: true}
+    )
     
   const HouseConnection = await connection.model('House', houseSchema);
 
-  const houseName = "house1";
+  const houseName = uuid.v4();
+  mongoose.set('debug', true);
 
   await HouseConnection.insertMany([
       {
@@ -24,16 +29,19 @@ const connection = mongoose.createConnection(
   ])
 
   // copy paste mongoose doc. example
-  const house = await HouseConnection.findOne({ houseName });
+  const house = await HouseConnection.findOne({ name: houseName });
   if (house.photos.length < 2) {
     throw new Error('House must have at least two photos!');
   }
 
-  const house2 = await HouseConnection.findOne({ houseName });
+  const house2 = await HouseConnection.findOne({ name: houseName });
   house2.photos = [];
-  house2.save();
+  house2.status = 'DECLINED'; // overwritten 
+  await house2.save();
 
   // Marks the house as 'APPROVED' even though it has 0 photos!
-  house.status = 'APPROVED';
-  house.save();
+  // btw did not overwrite the empty arrow but still set new field..?
+  house.status = 'APPROVED'; // saves it despite condition not being fulfilled
+  await house.save();
+  console.log("updated everything - no locking issue");
 })();
